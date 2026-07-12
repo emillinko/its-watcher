75291384c13197e5f49c16cc9f88abd617b84e73
emillinko <emillinko@outlook.jp>
2026-07-12 17:34:11 +0900

    Update check.js

--- check.js
+++ check.js
@@ -1,3 +1,25 @@
-const html = await page.content();
+const { chromium } = require('playwright');
 
-console.log(html.substring(0, 1000));
+(async () => {
+
+  const browser = await chromium.launch({
+    headless: true
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(
+    "REMOVED_ITS_KENPO_URL",
+    {
+      waitUntil: "networkidle",
+      timeout: 60000
+    }
+  );
+
+  console.log("Title:", await page.title());
+
+  console.log(await page.content().substring(0, 1000));
+
+  await browser.close();
+
+})();
