f4814f1ca097a2ae3abbb5fb9407f6591589d13e
emillinko <emillinko@outlook.jp>
2026-07-26 00:07:18 +0900

    Update check.js

--- check.js
+++ check.js
@@ -1,6 +1,24 @@
 const fs = require("fs");
 
-// HTML全部保存
-fs.writeFileSync("page.html", text);
+const url =
+  "REMOVED_ITS_KENPO_URL";
 
-console.log("HTML length =", text.length);
+(async () => {
+  const res = await fetch(url, {
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      "X-Requested-With": "XMLHttpRequest",
+      "Referer": "REMOVED_ITS_KENPO_URL"
+    }
+  });
+
+  console.log("status =", res.status);
+
+  const text = await res.text();
+
+  console.log("HTML length =", text.length);
+
+  fs.writeFileSync("page.html", text);
+
+  console.log(text.substring(0, 1000));
+})();
