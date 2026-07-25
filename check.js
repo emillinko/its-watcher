259b6aab4d8c1b3d61bf10b2fb8240505fd2ecf3
emillinko <emillinko@outlook.jp>
2026-07-25 23:46:41 +0900

    Update check.js

--- check.js
+++ check.js
@@ -1,21 +1,26 @@
+const fs = require("fs");
+
 const url =
   "REMOVED_ITS_KENPO_URL";
 
 (async () => {
   const res = await fetch(url, {
     headers: {
-      "User-Agent":
-        "Mozilla/5.0",
+      "User-Agent": "Mozilla/5.0",
       "X-Requested-With": "XMLHttpRequest",
-      "Referer": "REMOVED_ITS_KENPO_URL"
-    }
+      "Referer": "REMOVED_ITS_KENPO_URL",
+    },
   });
 
   console.log("status =", res.status);
 
   const text = await res.text();
 
-  require("fs").writeFileSync("page.html", text);
+  fs.writeFileSync("page.html", text);
+
+  console.log("===== HTML先頭 =====");
+  console.log(text.substring(0, 3000));
+  console.log("===== ここまで =====");
 
   console.log("○ =", text.includes("○"));
   console.log("△ =", text.includes("△"));
@@ -24,5 +29,3 @@ const url =
   console.log("富士屋 =", text.includes("フルーツパーク富士屋ホテル"));
   console.log("ラビスタ =", text.includes("ラビスタ富士河口湖"));
 })();
-
-console.log(text.substring(0, 3000));
